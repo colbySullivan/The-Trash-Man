b1350c94e41db3273b4f49ceabb0613f46e9a5ee
Colby Sullivan <sullivanc@southwestern.edu>
2023-12-04 19:53:26 -0600

    Setup menu scene

---
+++ source/Levels/menu.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class menu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Can select with ui arrows
+		GetNode<Button>("Quit").GrabFocus();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	private void _on_start_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://Levels/first_screen.tscn");
+	}
+
+
+	private void _on_quit_pressed()
+	{
+		GetTree().Quit();
+	}
+
+}
